243833ad1c9dea4dc1051c70086b850e2083a1cd
Roger <evolpirate@gmail.com>
2019-08-26 21:10:38 -0400

    adding test suite for root route

---
+++ test/courses.test.js
@@ -0,0 +1,12 @@
+// we will use supertest to test HTTP requests/responses
+const request = require("supertest");
+// we also need our app for the correct routes!
+const app = require("../app");
+
+describe("GET / ", () => {
+    it("should respond with a status of 200", async () => {
+      const response = await request(app).get("/");
+      
+      expect(response.statusCode).toBe(200);
+    });
+});
