ec4aeadcb3c28f895d971f763e38cb72251bb196
landmammal <evolpirate@gmail.com>
2020-02-03 01:13:19 -0500

    setting up passport on the application

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import "dotenv/config";
 import express from "express";
 import routes from "./routes";
+import passport from "passport";
+import bodyParser from "body-parser";
+import session from "express-session";
 
 // starting up app
 const app = express();
@@ -10,6 +13,13 @@ app.set("view engine", "ejs");
 
 //middleware
 app.use(express.static("./public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 
 // routing manager
 app.use(routes);
