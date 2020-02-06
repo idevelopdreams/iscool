ac861f7692b0f58c65cf200e0d399f96c47f8f5e
landmammal <evolpirate@gmail.com>
2020-02-05 22:22:01 -0500

    creating course table

--- models/course.js
+++ models/course.js
@@ -3,16 +3,29 @@ module.exports = (sequelize, DataTypes) => {
   const Course = sequelize.define(
     "Course",
     {
-      title: DataTypes.STRING,
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
       description: DataTypes.STRING,
       rating: DataTypes.STRING,
       video: DataTypes.STRING,
-      image: DataTypes.STRING
+      image: DataTypes.STRING,
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4
+      }
     },
     {}
   );
   Course.associate = function(models) {
     // associations can be defined here
+    User.belongsTo(models.User, {
+      foreignKey: "UserId",
+      onDelete: "CASCADE"
+    });
   };
   return Course;
 };
