c7acbf486f50bd0221fb63674d619e629a508dab
landmammal <evolpirate@gmail.com>
2020-02-07 03:56:18 -0500

    creating controller method to handle course creation

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,9 +1,13 @@
 // GET all courses for homepage
 exports.home = (req, res) => {
-    res.render('home');
-}
+  res.render("home");
+};
 
-// GET a single course 
+// GET a single course
 exports.singleCourse = (req, res) => {
-    res.render('course')
-}
+  res.render("course");
+};
+
+exports.createCourse = (req, res) => {
+  // logic
+};
