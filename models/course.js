2bb44cc31aa7f0eb79146bbd1f869d6f68d79022
landmammal <evolpirate@gmail.com>
2020-02-08 21:14:20 -0500

    creating course model

--- models/course.js
+++ models/course.js
@@ -1,32 +1,32 @@
 "use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Course = sequelize.define(
-    "Course",
-    {
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
-      description: DataTypes.STRING,
-      rating: DataTypes.STRING,
-      video: DataTypes.STRING,
-      image: DataTypes.STRING,
-      UserId: DataTypes.UUID,
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4
-      }
+  const Course = sequelize.define("Course", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
     },
-    {}
-  );
+    description: DataTypes.STRING,
+    rating: DataTypes.STRING,
+    video: DataTypes.STRING,
+    image: DataTypes.STRING,
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    }
+  });
   Course.associate = function(models) {
     // associations can be defined here
     Course.belongsTo(models.User, {
       foreignKey: "UserId",
       as: "creator",
       onDelete: "CASCADE"
+    });
+    Course.belongsToMany(models.User, {
+      through: "CourseRegistration",
+      foreignKey: "CourseId",
+      as: "students"
     });
   };
   return Course;
