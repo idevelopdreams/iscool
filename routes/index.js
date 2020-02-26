75d3c66b02981878a19aebbc4498198a777b2f6f
landmammal <evolpirate@gmail.com>
2020-02-26 01:50:58 -0500

    added module routes to our routes manager

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,15 @@
 import express from "express";
 import courses from "./courses";
 import user from "./user";
+import modules from "./modules";
 
 const router = express.Router();
 
 // routes for courses
 router.use(courses);
+
+//routes for modules
+router.use(modules);
 
 // routes for courses
 router.use(user);
