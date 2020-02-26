03f54e3de7cbf0d22b8ee10de180e98bba7b5322
landmammal <evolpirate@gmail.com>
2020-02-26 01:10:36 -0500

    module model with no need for a course

--- models/module.js
+++ models/module.js
@@ -1,11 +1,30 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Module = sequelize.define('Module', {
+  const Module = sequelize.define("Module", {
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4
+    },
     title: DataTypes.STRING,
-    overview: DataTypes.TEXT
-  }, {});
+    overview: DataTypes.TEXT,
+    CourseId: {
+      type: DataTypes.UUID,
+      allowNull: true,
+      references: {
+        model: "Courses",
+        key: "id"
+      }
+    }
+  });
   Module.associate = function(models) {
     // associations can be defined here
+    Module.belongsTo(models.User, {
+      foreignKey: "UserId",
+      as: "creator",
+      onDelete: "CASCADE"
+    });
   };
   return Module;
 };
