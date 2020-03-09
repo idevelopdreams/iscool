7c55dadfc428375eefb93e802020d05a912bbb07
landmammal <evolpirate@gmail.com>
2020-03-09 00:10:07 -0400

    updating lessons migration to use id as UUID

--- migrations/20200226070106-create-lesson.js
+++ migrations/20200226070106-create-lesson.js
@@ -4,9 +4,24 @@ module.exports = {
     return queryInterface.createTable("Lessons", {
       id: {
         allowNull: false,
-        autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4
+      },
+      UserId: {
+        type: Sequelize.UUID,
+        references: {
+          model: "Users",
+          key: "id"
+        }
+      },
+      ModuleId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: {
+          model: "Modules",
+          key: "id"
+        }
       },
       name: {
         type: Sequelize.STRING
