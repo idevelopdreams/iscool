b89d1b8c87a2f21d29e2035b605c4cc91cb931b0
landmammal <evolpirate@gmail.com>
2020-05-04 03:36:39 -0500

    begining with side bar collapse

--- public/javascript/player.js
+++ public/javascript/player.js
@@ -1,16 +1,14 @@
 $(document).ready(function () {
-    // load page with logo removed
-    $('#logo').toggle()
+  // load page with logo removed
+  $("#logo").toggle();
 
-    // load video player
-    const player = new Plyr('#player');
+  // load video player
+  const player = new Plyr("#player");
 
-    // toggle sidebar
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar').toggleClass('active');
+  // toggle sidebar
+  $("#sidebarCollapse").on("click", function () {
+    $("#sidebar").toggleClass("active");
 
-        $('#logo').toggle()
-    });
-
+    $("#logo").toggle();
+  });
 });
-
